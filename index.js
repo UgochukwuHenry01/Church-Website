c794f738d3b47a0c10ba20ffb3637441a3038933
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-02-19 20:11:50 +0100

    deleted javascript codes

--- index.js
+++ index.js
@@ -46,25 +46,3 @@ giveBtn.addEventListener('click', () => {
     // Code to handle the give button click event
     console.log('Give button clicked!');
 });
-
-
-//for my map to show
-// function initMap() {
-// const map = newgoogle.maps.Map(document.getElementById("map"),{
-// center:{ lat: 37.7749, lng: -122.4194 }, zoom: 13,
-// });
-// const marker = newgoogle.maps.Marker({
-//     position: { lat: 37.7859, lng: -122.4364 },
-// map: map,
-// title: "PRAISE COURT ASSEMBLY"
-// });
-// const infoWindow = newgoogle.maps.InfoWindow({
-// content: "<h2>PRAISE COURT ASSEMBLY</h2><p>Address:No.6 Faith Avenue</p>",
-// });
-// marker.addEventListener("click", () => {
-
-//     infoWindow.open(map,marker);
-// });
-// const searchBox = newgoogle.maps.places.searchBox(document.getElementById("search-input"))
-
-// }
