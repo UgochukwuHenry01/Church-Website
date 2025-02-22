31b02be4fd0cf55aee37c3096314bfe74fdd2078
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-02-22 17:05:59 +0100

    deleted gift button from javascript program

--- index.js
+++ index.js
@@ -39,26 +39,7 @@ function loadNavBar() {
 }
 loadNavBar();
 
-//button for giving
-// const giveBtn = document.querySelector('.give-btn');
 
-// giveBtn.addEventListener('click', () => {
-//     // Code to handle the give button click event
-//     console.log('Give button clicked!');
-// });
-
-// const giftBtn = document.querySelector('.gift-btn');
-// const paymentForm = document.querySelector('.payment-form');
-// const donateBtn = document.querySelector('.donate-btn');
-
-// giftBtn.addEventListener('click', () => {
-//   paymentForm.style.display = 'block';
-// });
-
-// donateBtn.addEventListener('click', () => {
-//   // Add payment processing logic here
-//   alert('Thank you for your donation!');
-// });
 
 
 // for payment form 
