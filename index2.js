02b1ce3ad2818471f1c88a6b34a50e5b0895afa0
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-02 20:42:10 +0100

    removed window.addEventListener from index2.js file

--- index2.js
+++ index2.js
@@ -1,13 +1,18 @@
 //codes for popup support
+const button = document.querySelector('.popup-button');
 const popupButton = document.querySelector('.popup-button');
 const popupContent = document.querySelector('.popup-content');
 
 popupButton.addEventListener('click', () => {
-  popupContent.classList.toggle('.show');
+  popupContent.classList.toggle('show');
+});
+
+window.addEventListener('load', () => {
+    button.classList.add('show');
 });
 
 document.addEventListener('click', (e) => {
   if (! popupButton.contains(e.target) && ! popupContent.contains(e.target)) {
-    popupContent.classList.remove('.show');
+    popupContent.classList.remove('show');
   }
 });
