fb9afb4b7d731488fe5fcfc41e0673a2997f8a40
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-04 01:57:14 +0100

    removed color from nav style2.css

--- index2.js
+++ index2.js
@@ -8,11 +8,40 @@ popupButton.addEventListener('click', () => {
 });
 
 window.addEventListener('load', () => {
-    button.classList.add('show');
+  button.classList.add('show');
 });
 
 document.addEventListener('click', (e) => {
-  if (! popupButton.contains(e.target) && ! popupContent.contains(e.target)) {
+  if (!popupButton.contains(e.target) && !popupContent.contains(e.target)) {
     popupContent.classList.remove('show');
   }
+});
+
+
+//select all buttons
+const buttons = document.querySelectorAll('button');
+//add evenlistener to each button
+buttons.forEach((button) => {
+  buttons.addEventListener('.click', () => {
+    //get background element
+    const background = document.querySelector('.background');
+
+    //add the blur effect to the background
+    background.classList.add('blur-background');
+
+    //also trigger the specific functionalityn for each buttonn here
+    if (button.classList.contains('need-support-button')) {
+      //trigger the chatbot interface or other functionality for the need support button
+    } else if (button.classList('select-language-button')) {
+      //trigger the language selection functionality
+    } else if (button.classList.contains('alter-call-button')) {
+      //trigger its functionality
+    } else if (button.classList.contains('first-time-worshipper-button')) {
+      //trigger its functionality
+    } else if (button.classList.contains('testimonies-button')) {
+
+    } else if (button.classList.contains('prayer-request-button')) {
+
+    }
+  });
 });
