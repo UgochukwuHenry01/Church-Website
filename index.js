84bc4ca26e28212078f406ba981322390bee7ca8
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-08 05:55:40 +0100

    removed const button from index.js

--- index.js
+++ index.js
@@ -3,29 +3,3 @@
 
 
 
-//Get all buttons
-const buttons = document.querySelectorAll('.button');
-
-// Add event listener to each button
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    // Get the form container associated with the button
-    const formContainer = button.nextElementSibling;
-
-    // Check if the form container is already visible
-    if (formContainer.style.display === 'block') {
-      // Hide the form container
-      formContainer.style.display = 'none';
-    } else {
-      // Show the form container
-      formContainer.style.display = 'block';
-      // Hide other form containers
-      buttons.forEach((otherButton) => {
-        if (otherButton !== button) {
-          const otherFormContainer = otherButton.nextElementSibling;
-          otherFormContainer.style.display = 'none';
-        }
-      });
-    }
-  });
-});
