33c08688d237d2ed0fd1d21f6fb445703610aa26
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-08 06:03:28 +0100

    removed body from style.css images

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+function toggleForm(formType) {
+  document.getElementById("userForm").style.display = "block";
+  document.getElementById("formTitle").innerText = formType;
+  document.getElementById("formPurpose").placeholder = "Write here...";
+  document.getElementById("languageSelect").style.display = formType === "Select a Language" ? "block" : "none";
+}
 
+function validateForm(event) {
+  event.preventDefault();
+  alert("You have successfully submitted your form!");
+}
 
 
 
