40522273e21c09ca7622e4d8ab33775fc5c83913
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-10 00:11:26 +0100

    removed .nav-link a from style.css

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 
+// for my navbars
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+const exitMenu = document.getElementById("exit-menu");
 
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+exitMenu.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+});
+
+
+//for radio broadcast
+function playRadio() {
+    document.getElementById("radioPlayer").play();
+}
+
+function pauseRadio() {
+    document.getElementById("radioPlayer").pause();
+}
+
+//for the slider
+let index = 0;
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img");
+
+function nextSlide() {
+    index++;
+    if (index >= images.length) {
+        index = 0;
+    }
+    slides.style.transform = `translateX(${-index * 20}%)`;
+}
+setInterval(nextSlide, 1000);
 
 
 
