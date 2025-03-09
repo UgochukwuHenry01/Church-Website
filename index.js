9f42941732bb72719c2e6c37dd1cbc55326ca051
UgochukwuHenry01 <ugochukwuhenryedeh@gmail.com>
2025-03-09 15:30:08 +0100

    deleted give.html

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function toggleForm(formType) {
-  document.getElementById("userForm").style.display = "block";
-  document.getElementById("formTitle").innerText = formType;
-  document.getElementById("formPurpose").placeholder = "Write here...";
-  document.getElementById("languageSelect").style.display = formType === "Select a Language" ? "block" : "none";
-}
 
-function validateForm(event) {
-  event.preventDefault();
-  alert("You have successfully submitted your form!");
-}
 
 
 
